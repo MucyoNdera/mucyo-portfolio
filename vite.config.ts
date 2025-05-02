690c0c439e0ce6b156d6451e31a628a2736dae4f
MucyoNdera <mucyo.nderat@gmail.com>
2025-05-03 01:34:41 +0200

    Update vite.config.ts with base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
-    base:"/mucyo-portfolio"
-
+    exclude: ['lucide-react']
   },
+  base: "/mucyo-portfolio",
 });
