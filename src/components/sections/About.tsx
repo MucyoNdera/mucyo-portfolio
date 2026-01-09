682a77efc69c8f88b39ef1458fcfcb4ed21ce9e3
MucyoNdera <mucyo.nderat@gmail.com>
2026-01-09 16:55:56 +0200

    Update Skills section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -8,18 +8,13 @@ export function About() {
         <div className="grid md:grid-cols-2 gap-12">
           <div className="space-y-6">
             <p className="text-lg text-gray-900">
-              My name is Mucyo Ndera Tuyizere.
-              I previously worked as a Climate Adaptation Fellow at GanzAfrica Organization, where I supported the design and implementation of community-driven, 
-              nature-based solutions using data-driven approaches to strengthen local climate adaptation efforts in Musanze District, Rwanda.
-               Before that, I served as a Research Assistant at the University of Rwanda’s Centre for Geographic Information Systems and Remote Sensing, 
-               contributing to projects that applied geospatial technologies to support natural resource management, forest landscape restoration, and spatial health analysis. 
-               In addition, during my time at the Centre, I provided training to students and professionals for almost one year, 
-               equipping them with practical GIS and remote sensing skills applicable to their professional roles.
+              I am an environmental planning professional with experience in climate adaptation, natural resource management, and applied geospatial analysis.
+               My work has included supporting community-driven, nature-based solutions in Musanze District through data-driven approaches to strengthen local climate adaptation,
+                as well as contributing to geospatial research on forest landscape restoration, spatial health, and natural resources management at the University of Rwanda’s Centre for Geographic Information Systems and Remote Sensing.
             </p>
             <p className="text-lg text-gray-900">
-              I hold a Bachelor of Science (Honors) degree in Geography with a specialization in Environmental Planning from the University of Rwanda.
-              My work is driven by a deep commitment to sustainable environmental practices, with a strong focus on natural resources management, 
-              sustainable agriculture, climate adaptation, ecosystem restoration, and community empowerment.
+              I hold a Bachelor of Science (Honours) degree in Geography with a specialization in Environmental Planning from the University of Rwanda, 
+              and my professional interests focus on sustainable natural resources management, climate adaptation, ecosystem restoration, and community empowerment.
             </p>
           </div>
             <div className="relative rounded-xl overflow-hidden shadow-xl transform transition-transform duration-500 hover:scale-[1.02]">
